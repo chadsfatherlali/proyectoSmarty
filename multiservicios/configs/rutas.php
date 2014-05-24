4fed6e6079457b61005078ca61f6a2c1895bdb88
Asistecnic <asistecnic@asistecnic.(none)>
2014-05-24 13:55:53 +0200

    Cambios en los archivos de configuracion

--- multiservicios/configs/rutas.php
+++ multiservicios/configs/rutas.php
@@ -18,31 +18,7 @@ $menuheader = array(
 $servicios = array(
 	#thermomix
 	#cafetereas
-	#maquinas de coser
-	#televisión
-	#equipos de sonido
-	#antenistas
-	#informática
-	#equipamiento hospitalario Y aparatología denttal
-	#equipamiento de gimnasio, relax y cintas de correr
-	#servicios técnicos para empresas
-	#centralitas telefónicas
-	#cámaras de frio y aire acondicionado
-	#calderas y calentadores
-	#informática
-	#domótca
-	#automatismos
-	#Puertas automáticas y de garage
-	#electricistas
-	#alarmas
-	#apertura de puertas y cerrajeria
-	#fontaneros
-	#averías de gas
-	#energía solar
-	#pintores y parquet
-	#persianas y cristaleros
-	#roulot
-	#servicios para el hogar
+	
 	#electrodomésticos
 	#aspiradoras
 	#averías de gas
@@ -61,6 +37,48 @@ $servicios = array(
 		"img" => "",
 		"resumen" => "",
 	),
+	"servicio-tecnico/maquinas-de-coser" => array(
+		"nombre" => "Máquinas de coser",
+		"link" => "servicio-tecnico/maquinas-de-coser",	
+		"img" => "",
+		"resumen" => "",
+	),
+	"servicio-tecnico/television" => array(
+		"nombre" => "Televisión",
+		"link" => "servicio-tecnico/television",
+		"img" => "",
+		"resumen" => "",	
+	),
+	"servicio-tecnico/equipos-de-sonido" => array(
+		"nombre" => "Equipos de sonido",
+		"link" => "servicio-tecnico/equipos-de-sonido",
+		"img" => "",
+		"resumen" => "",	
+	),
+	"servicio-tecnico/antenistas" => array(
+		"nombre" => "Antenistas",
+		"link" => "servicio-tecnico/antenistas",
+		"img" => "antenistas.jpg",
+		"resumen" => "Serviciio técnico, reparamos, instalamos y orientamos todo tipo de antenas de televisión",	
+	),
+	"servicio-tecnico/equipamiento-hospitalario" => array(
+		"nombre" => "Equipamiento hospitalario",
+		"link" => "servicio-tecnico/equipamiento-hospitalario",
+		"img" => "equipamiento-hospitalario.jpg",
+		"resumen" => "Servicio técnico de restauración y reparación de equipamiento hospitalrio.",	
+	),
+	"servicio-tecnico/equipamiento-de-gimnasio-y-relax" => array(
+		"nombre" => "Equipamiento de gimnasio y relax",
+		"link" => "servicio-tecnico/equipamiento-de-gimnasio-y-relax",
+		"img" => "equipamiento-de-gimnasio-y-relax.jpg",
+		"resumen" => "Reparación, mantenimiento y reacondicionamiento de aparatología cardiovascular, relax, welness, etc.",	
+	),
+	"servicio-tecnico/servicio-tecnico-empresas" => array(
+		"nombre" => "Servicio técnico empresas",
+		"link" => "servicio-tecnico/servicio-tecnico-empresas",
+		"img" => "servicio-tecnico-empresas.png",
+		"resumen" => "Servicio técnico para empresas damos soporte a todo tipo de incidencias empresariales.",	
+	),
 	"servicio-tecnico/centralitas-telefonicas" => array(
 		"title" => "Servicio técnico de Centralitas telefónicas Madrid, Toledo y poblaciones",
 		"descripcion" => "Servicio técnico, reparaciones, averías y mantenimiento de centralitas telefónicas en general",
@@ -69,6 +87,18 @@ $servicios = array(
 		"link" => "servicio-tecnico/centralitas-telefonicas",
 		"img" => "centralitas-telefonicas.jpg",
 		"resumen" => "Instalación, reparación y mantenimiento de centralitas, repetidoras de wifi.",
+	),
+	"servicio-tecnico/camaras-de-frio-y-aire-acondicionado" => array(
+		"nombre" => "Cámaras de frío",
+		"link" => "servicio-tecnico/camaras-de-frio-y-aire-acondicionado",
+		"img" => "camaras-de-frio.jpg",
+		"resumen" => "Servicio técnico y mantenimiento de camarás industriales de frío.",	
+	),
+	"servicio-tecnico/calderas-y-calentadores" => array(
+		"nombre" => "Calderas y calentadores",
+		"link" => "servicio-tecnico/calderas-y-calentadores",
+		"img" => "",
+		"resumen" => "",	
 	),
 	"servicio-tecnico/informatica" => array(
 		"title" => "Servicio técnico de Centralitas telefónicas Madrid, Toledo y poblaciones",
@@ -79,156 +109,138 @@ $servicios = array(
 		"img" => "informatica.jpg",
 		"resumen" => "Reparamos e instalamos todo tipo de ordenadores, limpieza de virus, mantenimineto en general.",	
 	),
-	2 => array(
+	"servicio-tecnico/domotica" => array(
 		"nombre" => "Domótica",
 		"link" => "servicio-tecnico/domotica",
 		"img" => "domotica.jpg",
 		"resumen" => "Reparamos e instalamos todo tipo de asistentes automáticos para el hogar, domótica en general.",	
 	),
-	3 => array(
-		"nombre" => "Cámaras de frío",
-		"link" => "servicio-tecnico/camaras-de-frio",
-		"img" => "camaras-de-frio.jpg",
-		"resumen" => "Servicio técnico y mantenimiento de camarás industriales de frío.",	
+	"servicio-tecnico/automatismos" => array(
+		"nombre" => "Automatismos",
+		"link" => "servicio-tecnico/automatismos",
+		"img" => "automatismos.jpg",
+		"resumen" => "Asistecnic multiservicios ofrece su servicio técnico de reparacíon, mantenimiento e intalamiento de automatísmos.",	
 	),
-	4 => array(
-		"nombre" => "Equipamiento de gimnasio y relax",
-		"link" => "servicio-tecnico/equipamiento-de-gimnasio-y-relax",
-		"img" => "equipamiento-de-gimnasio-y-relax.jpg",
-		"resumen" => "Reparación, mantenimiento y reacondicionamiento de aparatología cardiovascular, relax, welness, etc.",	
+	"servicio-tecnico/puertas-automáticas-y-de-garage" => array(
+		"nombre" => "Puertas automáticas y de garage",
+		"link" => "servicio-tecnico/puertas-automáticas-y-de-garage",
+		"img" => "",
+		"resumen" => "",	
 	),
-	5 => array(
-		"nombre" => "Servicio técnico empresas",
-		"link" => "servicio-tecnico/servicio-tecnico-empresas",
-		"img" => "servicio-tecnico-empresas.png",
-		"resumen" => "Servicio técnico para empresas damos soporte a todo tipo de incidencias empresariales.",	
+	"servicio-tecnico/electricistas" => array(
+		"nombre" => "Electricistas",
+		"link" => "servicio-tecnico/electricistas",
+		"img" => "electricistas.jpg",
+		"resumen" => "Servicio téncio, mantenimientos, reparaciones, y montajes de electricidad en general.",	
 	),
+	"servicio-tecnico/alarmas" => array(
+		"nombre" => "Alarmas",
+		"link" => "servicio-tecnico/alarmas",
+		"img" => "alarmas.jpg",
+		"resumen" => "Servicio técnico de instalación y reparación de todo tipo de alarmas para el hogar o empresas",	
+	),
+	"servicio-tecnico/aperturas-depuertas-y-cerrajeria" => array(
+		"nombre" => "Aperturas de puertas y cerrajería",
+		"link" => "servicio-tecnico/aperturas-depuertas-y-cerrajeria",
+		"img" => "",
+		"resumen" => "",	
+	),
+	"servicio-tecnico/fontaneros" => array(
+		"nombre" => "Fontaneros",
+		"link" => "servicio-tecnico/fontaneros",
+		"img" => "fontaneros.jpg",
+		"resumen" => "Servicio técnio y reparación de tuberias, atascos, y servico general de fontaneros.",	
+	),
+	"servicio-tecnico/averias-de-gas" => array(
+		"nombre" => "Averías de gas",
+		"link" => "servicio-tecnico/averias-de-gas",
+		"img" => "",
+		"resumen" => "",	
+	),
+	"servicio-tecnico/energia-solar" => array(
+		"nombre" => "Energia solar",
+		"link" => "servicio-tecnico/energia-solar",
+		"img" => "energia-solar.jpg",
+		"resumen" => "Instalación, reparación y mantenimiento, energías renovables, energía térmica, energía fotovoltaíca, geotérmica.",	
+	),
+	"servicio-tecnico/pintores-parquet" => array(
+		"nombre" => "Pintores - parquet",
+		"link" => "servicio-tecnico/pintores-parquet",
+		"img" => "pintores-parquet.jpg",
+		"resumen" => "Ofrecemos servicio de pintores e instalación de parquet para particulares, empresas.",	
+	),
+	"servicio-tecnico/persianas-y-cristaleros" => array(
+		"nombre" => "Persianas y cristaleros",
+		"link" => "servicio-tecnico/persianas-y-cristaleros",
+		"img" => "persianas-y-cristaleros.jpg",
+		"resumen" => "Reparación, instalación y mantenimiento de todo tipo de persianas, limpieza e instalación de cristales.",	
+	),
+	"servicio-tecnico/roulot" => array(
+		"nombre" => "Roulot",
+		"link" => "servicio-tecnico/roulot",
+		"img" => "roulot.jpg",
+		"resumen" => "Realizamos reparaciónes, revisiones, reformas, reconstrucción de piezas de plástico autocaravana o mómdulo y así darle solución a su problema.",	
+	),
+	"servicio-tecnico/servicios-para-el-hogar" => array(
+		"nombre" => "Servicios para el hogar",
+		"link" => "servicio-tecnico/servicios-para-el-hogar",
+		"img" => "servicios-para-el-hogar.jpg",
+		"resumen" => "Ofrecemos toda clase de servicios técnicos y reparaciones para el hogar, particulares y empresas.",	
+	),
+	"servicio-tecnico/electrodomesticos" => array(
+		"nombre" => "Electrodomésticos",
+		"link" => "servicio-tecnico/electrodomesticos",
+		"img" => "",
+		"resumen" => "",	
+	),
+
+
+
+
+
 	6 => array(
 		"nombre" => "Jardinería",
 		"link" => "servicio-tecnico/jardineria",
 		"img" => "jardineria.jpg",
 		"resumen" => "Realizamos instalaciones, reparaciones y mantenimientos de sus jardines.",	
 	),
-	7 => array(
-		"nombre" => "Persianas y cristaleros",
-		"link" => "servicio-tecnico/persianas-y-cristaleros",
-		"img" => "persianas-y-cristaleros.jpg",
-		"resumen" => "Reparación, instalación y mantenimiento de todo tipo de persianas, limpieza e instalación de cristales.",	
-	),
+	
 	8 => array(
 		"nombre" => "Toldos",
 		"link" => "servicio-tecnico/toldos",
 		"img" => "toldos.jpg",
 		"resumen" => "Instalación, reparación y mantenimiento de todo tipo de toldos, para hogar empresas e industrias.",	
 	),
-	9 => array(
-		"nombre" => "Electricistas",
-		"link" => "servicio-tecnico/electricistas",
-		"img" => "electricistas.jpg",
-		"resumen" => "Servicio téncio, mantenimientos, reparaciones, y montajes de electricidad en general.",	
-	),
-	10 => array(
-		"nombre" => "Antenistas",
-		"link" => "servicio-tecnico/antenistas",
-		"img" => "antenistas.jpg",
-		"resumen" => "Serviciio técnico, reparamos, instalamos y orientamos todo tipo de antenas de televisión",	
-	),
-	11 => array(
-		"nombre" => "Automatismos",
-		"link" => "servicio-tecnico/automatismos",
-		"img" => "automatismos.jpg",
-		"resumen" => "Asistecnic multiservicios ofrece su servicio técnico de reparacíon, mantenimiento e intalamiento de automatísmos.",	
-	),
-	12 => array(
-		"nombre" => "Energia solar",
-		"link" => "servicio-tecnico/energia-solar",
-		"img" => "energia-solar.jpg",
-		"resumen" => "Instalación, reparación y mantenimiento, energías renovables, energía térmica, energía fotovoltaíca, geotérmica.",	
-	),
-	13 => array(
-		"nombre" => "Fontaneros",
-		"link" => "servicio-tecnico/fontaneros",
-		"img" => "fontaneros.jpg",
-		"resumen" => "Servicio técnio y reparación de tuberias, atascos, y servico general de fontaneros.",	
-	),
-	14 => array(
-		"nombre" => "Servicios para el hogar",
-		"link" => "servicio-tecnico/servicios-para-el-hogar",
-		"img" => "servicios-para-el-hogar.jpg",
-		"resumen" => "Ofrecemos toda clase de servicios técnicos y reparaciones para el hogar, particulares y empresas.",	
-	),
+	
+	
+	
+	
 	15 => array(
 		"nombre" => "Mudánzas",
 		"link" => "servicio-tecnico/mudanzas",
 		"img" => "mudanzas.jpg",
 		"resumen" => "Ofrecemos un servicio de transporte y embalajes para hacer más comodo tu mudanzas, contamos con personal altamente cualificado.",	
 	),
-	16 => array(
-		"nombre" => "Pintores - parquet",
-		"link" => "servicio-tecnico/pintores-parquet",
-		"img" => "pintores-parquet.jpg",
-		"resumen" => "Ofrecemos servicio de pintores e instalación de parquet para particulares, empresas.",	
-	),
-	17 => array(
-		"nombre" => "Roulot",
-		"link" => "roulot",
-		"img" => "roulot.jpg",
-		"resumen" => "Realizamos reparaciónes, revisiones, reformas, reconstrucción de piezas de plástico autocaravana o mómdulo y así darle solución a su problema.",	
-	),
-	18 => array(
-		"nombre" => "Equipamiento hospitalario",
-		"link" => "servicio-tecnico/equipamiento-hospitalario",
-		"img" => "equipamiento-hospitalario.jpg",
-		"resumen" => "Servicio técnico de restauración y reparación de equipamiento hospitalrio.",	
-	),
-	19 => array(
-		"nombre" => "Alarmas",
-		"link" => "servicio-tecnico/alarmas",
-		"img" => "alarmas.jpg",
-		"resumen" => "Servicio técnico de instalación y reparación de todo tipo de alarmas para el hogar o empresas",	
-	),
-	20 => array(
-		"nombre" => "Televisión",
-		"link" => "servicio-tecnico/television",
-		"img" => "",
-		"resumen" => "",	
-	),
+
+
+	
+
 	21 => array(
 		"nombre" => "Cafeteras, thermomix y aspiradoras",
 		"link" => "servicio-tecnico/cafeteras-thermomix-y-aspiradoras",
 		"img" => "",
 		"resumen" => "",	
 	),
-	22 => array(
-		"nombre" => "Averías de gas",
-		"link" => "servicio-tecnico/averias-de-gas",
-		"img" => "",
-		"resumen" => "",	
-	),
-	23 => array(
-		"nombre" => "Máquinas de coser",
-		"link" => "servicio-tecnico/maquinas-de-coser",	
-		"img" => "",
-		"resumen" => "",
-	),
-	24 => array(
-		"nombre" => "Aperturas de puertas y cerrajería",
-		"link" => "servicio-tecnico/aperturas-depuertas-y-cerrajeria",
-		"img" => "",
-		"resumen" => "",	
-	),
+
+
+
 	25 => array(
 		"nombre" => "Reformas y construcción",
 		"link" => "servicio-tecnico/reformas-y-construccion",
 		"img" => "",
 		"resumen" => "",	
 	),
-	26 => array(
-		"nombre" => "Equipos de sonido",
-		"link" => "servicio-tecnico/equipos-de-sonido",
-		"img" => "",
-		"resumen" => "",	
-	),
+
 	27 => array(
 		"nombre" => "Porteros autmáticos",
 		"link" => "servicio-tecnico/porteros-automaticos",
@@ -241,30 +253,15 @@ $servicios = array(
 		"img" => "",
 		"resumen" => "",	
 	),
-	29 => array(
-		"nombre" => "Calderas y calentadores",
-		"link" => "servicio-tecnico/calderas-y-calentadores",
-		"img" => "",
-		"resumen" => "",	
-	),
-	30 => array(
-		"nombre" => "Electrodomésticos",
-		"link" => "servicio-tecnico/electrodomesticos",
-		"img" => "",
-		"resumen" => "",	
-	),
+
+	
 	31 => array(
 		"nombre" => "Desatrancos y pocería",
 		"link" => "servicio-tecnico/Desatrancos-y-poceria",
 		"img" => "",
 		"resumen" => "",	
 	),
-	32 => array(
-		"nombre" => "Puertas automáticas y de garage",
-		"link" => "servicio-tecnico/puertas-automáticas-y-de-garage",
-		"img" => "",
-		"resumen" => "",	
-	),
+
 	33 => array(
 		"nombre" => "Limpiezas y mantenimientos",
 		"link" => "servicio-tecnico/limpieza-y-mantenimientos",
